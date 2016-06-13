12620f38ef5cae086179cb9bad9fcde8d991836e
Eramit1708 <amit.goyal1708@gmail.com>
2016-06-13 18:37:57 +0530

    Initial commit

---
+++ app/src/main/java/com/yash/mytodoapp/MainActivity.java
@@ -0,0 +1,105 @@
+package com.yash.mytodoapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        addItems();
+
+        Fragment fragment=new ListFragment();
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.add(R.id.fragment_place,fragment);
+        //ft.replace(R.id.fragment_place, fragment);
+        ft.commit();
+
+    }
+
+
+    public void addItems() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final EditText myEditText = (EditText) findViewById(R.id.key);
+        ListView myListview = (ListView) findViewById(R.id.myList);
+        setSupportActionBar(toolbar);
+
+        //Get the ArrayList of to do Items
+        final ArrayList<String> todoItems = new ArrayList<String>();
+
+        // Create the Array Adapter to bind the array to the list view.
+
+        final ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+        //Bind the array adapter to the List View
+        myListview.setAdapter(aa);
+
+        myEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                    if ((keyCode == KeyEvent.KEYCODE_DPAD_CENTER) || (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                        todoItems.add(0, myEditText.getText().toString());
+                        aa.notifyDataSetChanged();
+                        myEditText.setText("");
+                        return true;
+                    }
+                return false;
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
